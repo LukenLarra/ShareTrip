4dc56b666b570b47fb0e3b0250a6e912847a6ab2
Luken Larrañaga Villar <llarranaga024@ikasle.ehu.eus>
2024-04-12 10:55:44 +0200

    Sequence Diagram

--- src/main/java/eus/ehu/shareTrip/uicontrollers/CreateRideController.java
+++ src/main/java/eus/ehu/shareTrip/uicontrollers/CreateRideController.java
@@ -171,7 +171,6 @@ public class CreateRideController implements Controller{
             try {
                 businessLogic.acceptRequest(selectedRequest.getReservationCode());
                 refreshRideRequests(event);
-                selectedRequest.setStatus("Accepted");
                 messageRequest.setText("Request accepted successfully");
                 messageRequest.setStyle("-fx-text-fill: white; -fx-background-color: green; -fx-background-radius: 5px; -fx-text-radius: 5px;");
                 txtRequestCode.clear();
@@ -196,7 +195,6 @@ public class CreateRideController implements Controller{
             try {
                 businessLogic.rejectRequest(requestCode);
                 refreshRideRequests(event);
-                selectedRequest.setStatus("Rejected");
                 messageRequest.setText("Request rejected successfully");
                 messageRequest.setStyle("-fx-text-fill: white; -fx-background-color: green; -fx-background-radius: 5px; -fx-text-radius: 5px;");
                 txtRequestCode.clear();
@@ -208,7 +206,6 @@ public class CreateRideController implements Controller{
             try {
                 businessLogic.rejectRequest(selectedRequest.getReservationCode());
                 refreshRideRequests(event);
-                selectedRequest.setStatus("Rejected");
                 messageRequest.setText("Request rejected successfully");
                 messageRequest.setStyle("-fx-text-fill: white; -fx-background-color: green; -fx-background-radius: 5px; -fx-text-radius: 5px;");
                 txtRequestCode.clear();
